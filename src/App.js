2cc5b43f0f31a0def8e4be3e570630ea87e35ce9
Advik Sinha <advsinha112@gmail.com>
2023-11-19 01:51:27 +0530

    add new product form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import LandingPage from "./components/LandingPage/LandingPage";
+import LandingPage from "./pages/LandingPage/LandingPage";
 
 function App() {
   return (
