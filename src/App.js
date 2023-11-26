c268b83904af50b62c93ff2ff6ea2733ee1c5345
Pranathi Voora <pranathivoora2@gmail.com>
2023-11-26 16:01:18 +0530

    app.js is now error page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import LandingPage from "./components/LandingPage/LandingPage";
+//import LandingPage from "./components/LandingPage/LandingPage";
+import ErrorPage from "./pages/Error/ErrorPage";
 
 function App() {
   return (
     <div className="App">
-      <MySalesPage />
+      <ErrorPage />
     </div>
   );
 }
